033671b7dfd5602b814d3790a37cfe17de42dc0f
sikr <simon.kruska@googlemail.com>
2021-10-26 11:46:47 +0200

    Again remove negative offset for daily metrics as graphite behaves different  than expected

--- js/hmsrv.js
+++ js/hmsrv.js
@@ -672,7 +672,7 @@ function handleEnergyCounterDailyDiff() {
             graphiteCacheValuesFull.push({
               path: 'home.cellar.utility_room.electric_meter.energy_counter_by_day',
               value: diff,
-              timestamp: timestamp - 86400000
+              timestamp: timestamp
             });
 
             store.datapoints[3177].value = datapoints[3177].Value;
